285033e6a4cac89111e907685eb577c86702ebd0
Ashutosh <Ashutosh@192.168.0.10>
2019-06-09 03:35:24 +0530

    5th Spring Boot Application

---
+++ src/main/java/com/app/runner/AppRunnerThree.java
@@ -0,0 +1,17 @@
+package com.app.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(2)
+public class AppRunnerThree implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("From - runner B");
+	}
+
+}
